66d99852a6a0b94ba289a86718f778e47a610be4
Sayaja <35453348+Sayaja@users.noreply.github.com>
2019-02-20 16:35:28 +0100

    Random location of shot implemented

--- app/src/main/java/com/axelweinz/hockeyarlivefeed/MainActivity.java
+++ app/src/main/java/com/axelweinz/hockeyarlivefeed/MainActivity.java
@@ -1,11 +1,15 @@
 package com.axelweinz.hockeyarlivefeed;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.filament.Box;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
@@ -17,13 +21,20 @@ import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 import com.google.ar.sceneform.ux.TwistGestureRecognizer;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    //private ModelRenderable andyRenderable;
+    private ModelRenderable andyRenderable;
     private ModelRenderable hockeyRinkRenderable;
     private ArFragment arFragment;
+    private HitResult firstHit;
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Vector3 rinkPos;
+
     private Integer modelCount = 0;
+    private Vector3 shotPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +53,25 @@ public class MainActivity extends AppCompatActivity {
                             return null;
                         });
 
+        ModelRenderable.builder()
+                .setSource(this, R.raw.andy)
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Log.e(TAG, "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+
         arFragment.setOnTapArPlaneListener(
                 (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    firstHit = hitResult;
+
                     if (hockeyRinkRenderable == null) {
                         return;
                     }
                     if (modelCount == 1) { // Limit to 1 model
+                        shot();
                         return;
                     }
 
@@ -58,20 +82,65 @@ public class MainActivity extends AppCompatActivity {
 
                     // Create the transformable andy and add it to the anchor.
                     TransformableNode hockeyRink = new TransformableNode(arFragment.getTransformationSystem());
-                    hockeyRink.setParent(anchorNode);
                     hockeyRink.setRenderable(hockeyRinkRenderable);
 
                     // Set correct rotation of model, then disable rotation with twist
                     hockeyRink.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), -90f));
                     hockeyRink.getRotationController().setEnabled(false);
-                    hockeyRink.setLocalScale(new Vector3(0.5f, 0.5f, 0.5f));
-                    Vector3 pos = hockeyRink.getLocalPosition();
-                    Vector3 temp = new Vector3(pos.x + 0, pos.y + (pos.y+1)/10f, pos.z + 0);
-                    hockeyRink.setLocalPosition(temp);
+                    hockeyRink.getTranslationController().setEnabled(false);
+                    hockeyRink.getScaleController().setMinScale(0.4f);
+                    hockeyRink.getScaleController().setMaxScale(2.0f);
+                    hockeyRink.setLocalScale(new Vector3(0.8f, 0.8f, 0.8f));
+                    rinkPos = hockeyRink.getLocalPosition();
+                    Vector3 temp = new Vector3(rinkPos.x + 0, rinkPos.y + (rinkPos.y+0)/10f, rinkPos.z + 0);
+                    //hockeyRink.setLocalPosition(temp);
+
+                    hockeyRink.setParent(anchorNode);
+
+                    //shot();
 
                     hockeyRink.select();
 
                     modelCount += 1;
                 });
+    }
+
+    /** Called when the user taps the shot button */
+    public void shot() {
+
+        // Calculate a random position of the shot
+        float minZ = -0.50f;
+        float maxZ = -0.20f;
+        Random r = new Random();
+        float rZ = minZ + r.nextFloat() * (maxZ - minZ);
+        float minX = -0.35f;
+        float maxX = 0.35f;
+        r = new Random();
+        float rX = minX + r.nextFloat() * (maxX - minX);
+        shotPos = new Vector3(rinkPos.x + rX, rinkPos.y + 0, rinkPos.z + rZ);
+
+        // Create the Anchor.
+        Anchor anchor = firstHit.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        // Create the transformable andy and add it to the anchor.
+        TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+        andy.setRenderable(andyRenderable);
+
+        andy.getScaleController().setMinScale(0.1f);
+        andy.getScaleController().setMaxScale(2.0f);
+        andy.setLocalScale(new Vector3(0.2f, 0.2f, 0.2f));
+        //Vector3 pos = andy.getLocalPosition();
+        //Vector3 temp = new Vector3(pos.x + 0, pos.y + (pos.y+0)/10f, pos.z + 0);
+        andy.setLocalPosition(shotPos);
+
+        andy.setParent(anchorNode);
+
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
     }
 }
